b1375daf1084a5c4d3c5aec96e734f3c5a8cd5b2
AutreTemps <nik.shigaev@mail.ru>
2021-04-03 02:24:41 +0300

    Parser added

--- parser.py
+++ parser.py
@@ -1,10 +1,13 @@
 import numpy as np
 
 def get_set():
-    with open("dataset.dat") as data:
-        s = data.readline()
+    s = data.readline()
     correct_ans = int(s[0]) - 1
     s = s[4 : len(s)]
     s = list(map(float, s.split()))
     res = np.array([s[::2], s[1::2]])
     return correct_ans, res
+
+
+with open("dataset.dat") as data:
+    print(get_set()) #вызывать только при заранее открытом файле
